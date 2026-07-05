06cef16302892758d976fe019be0012fa89cbcce
kimkangwook <ices17@inha.edu>
2026-07-05 17:12:26 +0900

    feat/fix: 각 노드마다 print 추가 및 main 함수 작성

--- mail_agent/agent.py
+++ mail_agent/agent.py
@@ -28,11 +28,16 @@ class MailAgent:
         return builder.compile()
 
     def _mail_classification(self, state: mailState): # private
-        
+        print("STEP 1: LLM이 메일을 분류 중입니다. 잠시만 기다려주세요...")
+
         prompt = f"""
         당신은 메일 제목을 분류하는 분류기입니다.
         메일 제목을 보고 클래스를 분류하세요.
         클래스는 다음과 같습니다: 'normal', 'abnormal'
+
+        normal은 사용자가 확인해야하는 메일을 의미하고,
+        abnormal은 스팸이나 광고와 같이 사용자가 확인할 필요 없는 메일입니다.
+
         메일 제목:{state['mail_title']}
         응답 형식은 무조건 'normal' 혹은 'abnormal'로만 응답해주세요.
         응답 형식: normal or abnormal
@@ -40,15 +45,19 @@ class MailAgent:
 
         response = self.llm.invoke(prompt)
         if (response.content == 'normal'):
+            print("사용자가 읽어야 하는 메일 입니다.")
             return {"mail_class": 'normal'}
         
         if (response.content == 'abnormal'):
+            print("사용자가 읽을 필요가 없는 메일 입니다.")
             return {"mail_class": 'abnormal'}
         
+        print("메일 분류에 실패하였습니다.")
         return {"mail_class": 'error', "isSuccess": False}
 
 
     def _kakaotalkSend(self, state: mailState): # priavte
+        print("STEP 2: 카카오톡으로 알림을 보내는 중입니다. 잠시만 기다려주세요...")
         """
         카카오톡 메시지 전송 노드
         """
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from mail_agent.agent import MailAgent
 
 agent = MailAgent()
-
-# 그래프 이미지 저장
-graph_image = agent.graph.get_graph().draw_mermaid_png()
-with open("graph.png", "wb") as f:
-    f.write(graph_image)
+result = agent.run("당신의 계정이 해킹되었습니다")
+print(result)
