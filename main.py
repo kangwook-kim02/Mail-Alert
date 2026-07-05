06c27483eefd914fb029e1893a854cd5582c7ddb
kimkangwook <ices17@inha.edu>
2026-07-05 18:03:08 +0900

    fix/feat: main 함수에 메일 + 에이전트 연동 / 임시 로컬 저장소 생성

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
 from mail_agent.mail_fetcher import fetch_naver_mails, save_sent_id
+from mail_agent.agent import MailAgent
 
-mails = fetch_naver_mails()
+agent = MailAgent() # agent 연결
 
-for mail in mails:
-    print(f"제목: {mail['subject']}")
-    print(f"발신자: {mail['sender']}")
+def job():
+    mails = fetch_naver_mails()
     
-    # 나중에 여기서 agent.run() 호출
-    
-    save_sent_id(mail['mid'])  # 처리 완료 기록
+    for mail in mails:
+        print(f"제목: {mail['subject']}")
+        print(f"발신자: {mail['sender']}")
+        
+        # 나중에 여기서 agent.run() 호출
+        agent.run(mail['sender'], mail['subject'])
+        
+        save_sent_id(mail['mid'])
+
+scheduler = BlockingScheduler()
+scheduler.add_job(job, "interval", minutes=5)  # 5분마다 실행
+
+print("스케줄러 시작!")
+scheduler.start()
