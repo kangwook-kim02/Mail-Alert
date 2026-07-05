82f7bd769048056265734882381eb9dd51962610
kimkangwook <ices17@inha.edu>
2026-07-05 17:46:42 +0900

    fix:main 함수 수정

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-# main.py
-from mail_agent.mail_fetcher import fetch_naver_mails
+from mail_agent.mail_fetcher import fetch_naver_mails, save_sent_id
 
 mails = fetch_naver_mails()
-print(mails)
+
+for mail in mails:
+    print(f"제목: {mail['subject']}")
+    print(f"발신자: {mail['sender']}")
+    
+    # 나중에 여기서 agent.run() 호출
+    
+    save_sent_id(mail['mid'])  # 처리 완료 기록
